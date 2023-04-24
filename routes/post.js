c75e5d8c2b3996a25ca67b16b73eded8c5ed26ee
js <js@jsui-noteubug.local>
2023-04-25 01:05:59 +0900

    update: post.js21

--- routes/post.js
+++ routes/post.js
@@ -101,7 +101,7 @@ router.post("/", isLoggedIn, upload.none(), async (req, res, next) => {
   }
 });
 
-router.post("/images", isLoggedIn, upload.array("Images"), (req, res, next) => {
+router.post("/images", isLoggedIn, upload.array("image"), (req, res, next) => {
   // POST /post/images
   console.log(req.files);
   res.json(req.files.map((v) => v.location.replace(/\/original\//, "/thumb/")));

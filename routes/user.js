9124561da114017d312ce02c6086a58b7367b223
js <js@172.20.nate.com>
2023-04-28 16:42:12 +0900

    bug follow

--- routes/user.js
+++ routes/user.js
@@ -269,12 +269,12 @@ router.patch("/nickname", isLoggedIn, async (req, res, next) => {
 router.patch("/:userId/follow", isLoggedIn, async (req, res, next) => {
   // PATCH /user/1/follow
   try {
-    const user = await User.findOne({ where: { id: req.params.UserId } });
+    const user = await User.findOne({ where: { id: req.params.userId } });
     if (!user) {
       res.status(403).send("없는 사람을 팔로우하려고 하시네요?");
     }
     await user.addFollowers(req.user.id);
-    res.status(200).json({ UserId: parseInt(req.params.UserId, 10) });
+    res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
   } catch (error) {
     console.error(error);
     next(error);

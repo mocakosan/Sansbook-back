5a4db479881272c6703512dd7ea422b3d7035bef
js <js@172.20.nate.com>
2023-04-28 14:54:36 +0900

    udpate: post.js image thumb

--- routes/post.js
+++ routes/post.js
@@ -102,7 +102,7 @@ router.post("/", isLoggedIn, upload.none(), async (req, res, next) => {
 router.post("/images", isLoggedIn, upload.array("image"), (req, res, next) => {
   // POST /post/images
   console.log(req.files);
-  res.json(req.files.map((v) => v.location));
+  res.json(req.files.map((v) => v.location.replace(/\/original\//, "/thumb/")));
 });
 
 router.post("/:postId/retweet", isLoggedIn, async (req, res, next) => {

ca179fbed087b5a552441fb2852650bda095969d
js <js@jsui-noteubug.local>
2023-05-02 12:04:56 +0900

    bug korean name file2

--- routes/post.js
+++ routes/post.js
@@ -31,8 +31,8 @@ const upload = multer({
     key(req, file, cb) {
       cb(
         null,
-        `original/${Date.now()}_${path.encodeURIComponent(
-          basename(file.originalname)
+        `original/${Date.now()}_${encodeURIComponent(
+          path.basename(file.originalname)
         )}`
       );
     },
